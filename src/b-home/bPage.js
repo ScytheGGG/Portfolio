a6d6b23125089ea27edf7e732aadb596146659e5
KarlTammehoid <tammehoidkarl@gmail.com>
2024-04-30 19:26:30 +0300

    Navbar Tweaks

--- src/b-home/bPage.js
+++ src/b-home/bPage.js
@@ -16,7 +16,6 @@ export default function BPage() {
                     <p>ABOUT</p>
                     <p>EXPERIENCE</p>
                     <p>PROJECTS</p>
-                    <p>CONTACT</p>
                 </div>
                 <div className="bph--images">
                     <a href="https://github.com/scytheggg" target="_blank" rel="noreferrer">
@@ -24,6 +23,9 @@ export default function BPage() {
                     </a>
                     <a href="https://www.linkedin.com/in/tammehoidkarl/" target="_blank" rel="noreferrer">
                         <img className="linkedin--logo" src="./photos/linkedin-logo.png" alt="LinkedIn"></img>
+                    </a>
+                    <a href="mailto:tammehoidkarl@gmail.com?subject=Subject%20of%20the%20email&body=Body%20of%20the%20email" target="_blank" rel="noreferrer">
+                        <img class="mail--logo" src="./photos/mail-logo.png" alt="Mail"></img>
                     </a>
                 </div>
             </div>
