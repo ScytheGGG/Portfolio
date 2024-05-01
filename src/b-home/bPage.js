f976786ab4dd81d6167012de7f40505f0cd80b90
KarlTammehoid <tammehoidkarl@gmail.com>
2024-05-01 18:58:51 +0300

    Socials

--- src/b-home/bPage.js
+++ src/b-home/bPage.js
@@ -28,24 +28,24 @@ export default function BPage() {
                     <p>PROJECTS</p>
                 </div>
                 <div className="bph--images">
-                    <a href="https://github.com/scytheggg" target="_blank" rel="noreferrer">
-                        <div className="github--logo">
-                            <img className="filtered github--filtered" src="./photos/github-logo-filtered.png" alt="Github Filtered" />
-                            <img className="normal github--normal" src="./photos/github-logo.png" alt="Github Normal" />
-                        </div>
-                    </a>
-                    <a href="https://www.linkedin.com/in/tammehoidkarl/" target="_blank" rel="noreferrer">
-                        <div className="linkedin--logo">
-                            <img className="filtered linkedin--filtered" src="./photos/linkedin-logo-filtered.png" alt="LinkedIn Filtered" />
-                            <img className="normal linkedin--normal" src="./photos/linkedin-logo.png" alt="LinkedIn Normal" />
-                        </div>
-                    </a>
-                    <a href="mailto:tammehoidkarl@gmail.com" target="_blank" rel="noreferrer">
-                        <div className="mail--logo">
-                            <img className="filtered mail--filtered" src="./photos/mail-logo-filtered.png" alt="Mail Filtered" />
-                            <img className="normal mail--normal" src="./photos/mail-logo.png" alt="Mail Normal" />
-                        </div>
-                    </a>
+                <a href="https://github.com/scytheggg" target="_blank" rel="noreferrer">
+        <div className="github--logo">
+            <img className="filtered github--filtered" src="./photos/github-logo-filtered.png" alt="Github Filtered" width="24" height="23" />
+            <img className="normal github--normal" src="./photos/github-logo.png" alt="Github Normal" width="24" height="23" />
+        </div>
+    </a>
+    <a href="https://www.linkedin.com/in/tammehoidkarl/" target="_blank" rel="noreferrer">
+        <div className="linkedin--logo">
+            <img className="filtered linkedin--filtered" src="./photos/linkedin-logo-filtered.png" alt="LinkedIn Filtered" width="22" height="22" />
+            <img className="normal linkedin--normal" src="./photos/linkedin-logo.png" alt="LinkedIn Normal" width="22" height="22" />
+        </div>
+    </a>
+    <a href="mailto:tammehoidkarl@gmail.com" target="_blank" rel="noreferrer">
+        <div className="mail--logo">
+            <img className="filtered mail--filtered" src="./photos/mail-logo-filtered.png" alt="Mail Filtered" width="25" height="22" />
+            <img className="normal mail--normal" src="./photos/mail-logo.png" alt="Mail Normal" width="25" height="22" />
+        </div>
+    </a>
                 </div>
             </div>
             <div className="bPage--right">
